24756c58541732512c647aec74b45fb0e18d9b4a
JeremiahJacob261 <jeremiahjacob261>
2022-06-23 01:02:31 +0100

    rm paystack due to few heroku err

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from './components/footer.js';
 import Products from './components/Products';
 import Admin from './admin/Admin';
 import Flutterwave from './components/flutterwave';
-import Paystack from './components/paystack';
+
 function App() {
   return (
     <div className="App">
@@ -18,7 +18,6 @@ function App() {
    <Route path="/product" element={<Products/>}/>
    <Route path="/admin" element={<Admin/>}/>
    <Route path="/pay" element={<Flutterwave/>}/>
-   <Route path="/pay2" element={<Paystack/>}/>
  </Routes>
  <Footer/>
   </Router>
