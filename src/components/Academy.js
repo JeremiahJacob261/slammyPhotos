14ea13d9790e011d8bbdbb6464372e951af2e043
JeremiahJacob261 <jeremiahjacob261>
2022-06-23 14:21:48 +0100

    bn

--- src/components/Academy.js
+++ src/components/Academy.js
@@ -1,9 +1,14 @@
 import React from "react";
+import {motion} from 'framer-motion'
 function Academy(){
 return(
-    <div>
+    <motion.div
+    initial={{width:0,opacity:0}}
+      animate={{width:"100%",opacity:1}}
+      exit={{x:window.innerWidth,transition:{duration:0.1},opacity:0}}
+    >
         <h2>learn</h2>
-    </div>
+    </motion.div>
 );
 }
 export default Academy;
