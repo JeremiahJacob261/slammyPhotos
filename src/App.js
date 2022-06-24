77dbf1e4f67b43b67f2e4bd5b01ec5247c3f97af
JeremiahJacob261 <jeremiahjacob261>
2022-06-24 07:57:49 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from './components/Home.js'
