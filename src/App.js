e9092eec43ffc3d870f8fbfff96bd2271b911f88
JeremiahJacob261 <jeremiahjacob261>
2022-06-25 07:36:59 +0100

    null2

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Products from './pages/Products';
+import Header from './pages/header';
+import Footer from './pages/footer';
 function App() {
   return (
     <div className="App">
   <Router>
+    <Header/>
  <Routes>
    <Route path="/" element={<Home/>}/>
+   <Route path="/product" element={<Products/>}/>
  </Routes>
+ <Footer/>
   </Router>
     </div>
   );
