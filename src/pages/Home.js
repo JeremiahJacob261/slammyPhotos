82d66fcda09ba375a40b0c0ec2d965c8757aa79e
JeremiahJacob261 <jeremiahjacob261>
2022-06-26 20:35:22 +0100

    3b fast loading pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,7 +27,7 @@ function Home () {
     </Col>
     <Col class="home-image-side-col" sm={5}>
     <div id="home-image-side">
-    <img id="side" src={landing} alt="landing"  whileHover={{ scale: 1.1 }}/>
+    <img id="side" src={landingsm} alt="landing"  whileHover={{ scale: 1.1 }}/>
     </div>
     </Col>
     </Row>
