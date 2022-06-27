4e30b6740dbd782544907c068b9bc1fe652caff9
JeremiahJacob261 <jeremiahjacob261>
2022-06-27 09:22:08 +0100

    update on designing

--- src/pages/product-type.js
+++ src/pages/product-type.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Col,Container,Row} from 'react-bootstrap';
+import {Col,Container,Row,Stack} from 'react-bootstrap';
 import lut1 from './photos/lut1.JPG';
 import lut2 from './photos/lut2.webp'
 import lut3 from './photos/lut3.JPG'
@@ -8,20 +8,21 @@ function Type(){
 return(
     <div className="type">
 <Container>
-<Row>
-    <Col sm={3}> <div id="typesa">
-      
-        <h3 id="h3a">hello</h3>
-        <img src={lut1} alt=""/>
-        </div> </Col>
-    <Col sm={3}><div  id="typesb">
-    <h3 id="h3b">hello</h3>
+<Row className="types">
+<Stack direction="horizontal" gap={3}>
+    <Col sm={4}><div  id="typesb">
+    <h3 id="h3b">PDF</h3>
         <img src={lut2} alt=""/>
         </div></Col>
-    <Col sm={3}><div  id="typesc">
-    <h3 id="h3c">hello</h3>
+        <Col sm={4}> <div id="typesa">
+       <h3 id="h3a">Vidoes</h3>
+      <img src={lut1} alt=""/>
+      </div> </Col>
+    <Col sm={4}><div  id="typesc">
+    <h3 id="h3c">Presets</h3>
         <img src={lut3} alt=""/>
         </div></Col>
+        </Stack>
         </Row>
 </Container>
     </div>
